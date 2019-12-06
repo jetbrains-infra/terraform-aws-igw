69a24156fd45954d466bb0eef41d1a57c916b4e7
Nikita Koshcheev <nikita.koshcheev@jetbrains.com>
2019-12-06 13:25:31 +0300

    syntax (interpolation) changes to support terraform 0.12

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${local.vpc_id}"
+  vpc_id = local.vpc_id
 }
 
 resource "aws_route_table" "public_to_internet" {
-  vpc_id = "${local.vpc_id}"
+  vpc_id = local.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${local.igw_id}"
+    gateway_id = local.igw_id
   }
 
-  tags {
+  tags = {
     Name    = "Internet access"
-    Project = "${local.project}"
+    Project = local.project
   }
 }
