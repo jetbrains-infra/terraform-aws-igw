e72c65acddc5d93e503c46fdfa90edad8237cd36
Sergei Ugdyzhekov <sergei.ugdyzhekov@jetbrains.com>
2020-07-18 21:15:58 +0200

    fix: wrong link to the module

--- variables.tf
+++ variables.tf
@@ -7,10 +7,10 @@ variable "tags" {
 }
 
 locals {
-  vpc_id  = var.vpc_id
-  igw_id  = aws_internet_gateway.default.id
-  tags    = merge({
-    Module = "Internet Gateway"
-    ModuleSource    = "https://github.com/jetbrains-infra/terraform-aws-vpc/"
+  vpc_id = var.vpc_id
+  igw_id = aws_internet_gateway.default.id
+  tags   = merge({
+    Module       = "Internet Gateway"
+    ModuleSource = "https://github.com/jetbrains-infra/terraform-aws-igw/"
   }, var.tags)
 }
